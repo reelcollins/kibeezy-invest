5adf3ab096b47f1b1f7dfe6c599efb826e5da328
reelcollins <elijahcollins254@gmail.com>
2023-11-27 15:24:33 +0300

    removed signin

--- components/common/SocialButtons.tsx
+++ components/common/SocialButtons.tsx
@@ -8,10 +8,10 @@ export default function SocialButtons() {
 	return (
 		<div className='flex justify-between items-center gap-2 mt-5'>
 			<SocialButton provider='google' onClick={continueWithGoogle}>
-				<ImGoogle className='mr-3' /> Google Signin
+				<ImGoogle className='mr-3' /> Google
 			</SocialButton>
 			<SocialButton provider='facebook' onClick={continueWithFacebook}>
-				<ImFacebook className='mr-3' /> Facebook Signin
+				<ImFacebook className='mr-3' /> Facebook
 			</SocialButton>
 		</div>
 	);
