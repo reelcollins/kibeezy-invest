eba2e7dbcabdec0c8cd120e1c02f2eebb89fb6ef
reelcollins <elijahcollins254@gmail.com>
2023-12-19 17:49:24 +0300

    again

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -39,58 +39,6 @@ export default function Page() {
                 </Box>
 
             </Box>
-
-            {/* <Link
-                href="/map"
-                style={{
-                position: "fixed",
-                bottom: "20px",
-                left: "50%",
-                transform: "translateX(-50%)",
-                zIndex: 10,
-                }}
-            >
-                <FaMapMarkedAlt height="30" width="30" />
-            </Link>
-            <Link
-                href="/shorts"
-                style={{
-                position: "fixed",
-                bottom: "20px",
-                left: "50%",
-                transform: "translateX(-50%)",
-                zIndex: 10,
-                }}
-            >
-                <MdSmartDisplay height="30" width="30" />
-            </Link> */}
-
-            {/* <Box className="fixed bottom-0 left-0 right-0 flex justify-center">
-              
-                
-                    <Link
-                        href="/map"
-                        className="text-4xl mr-4 hover:bg-gray-200"
-                    >
-                        <FaMapMarkedAlt />
-                    </Link>
-                    <Link href="/shorts" className="text-4xl hover:bg-gray-200">
-                        <MdSmartDisplay />
-                    </Link>
-                
-    
-            
-                <Link
-                href="/map"
-                className="text-3xl mr-1 hover:bg-gray-200" // smaller icon size, margin, and hover background
-                >
-                <FaMapMarkedAlt />
-                </Link>
-                <Link href="/shorts" className="text-3xl hover:bg-gray-200">
-                <MdSmartDisplay />
-                </Link>
-            
-            </Box> */}
             {/* <Box
                 className="fixed left-0 right-0 flex justify-center bottom-5"
                 >
@@ -106,7 +54,7 @@ export default function Page() {
             <Box
                 className="fixed left-0 right-0 flex justify-center bottom-5"
                 >
-                <Box className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-2">
+                <Box className="grid grid-cols-2 sm:grid-cols-1 gap-4 sm:gap-2">
                     <Link
                     href="/map"
                     className="text-4xl mr-4 sm:w-1/2 sm:text-3xl hover:bg-gray-200"
