c86b7eafccc769208a298dac7986c1efa5636af5
reelcollins <elijahcollins254@gmail.com>
2023-12-26 08:27:04 +0300

    justify-between

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -70,7 +70,7 @@ export default function Navbar() {
 		<Disclosure as='nav' className='bg-gray-800'>
 			{({ open }) => (
 				<>
-					<div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
+					<div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 justify-between'>
 						<div className='relative flex h-16 items-center justify-between'>
 							<div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
 								<Disclosure.Button className='inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
