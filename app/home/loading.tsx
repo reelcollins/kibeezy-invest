7c564a6f8685d6e8930dab10cb188a0b524e1562
reelcollins <elijahcollins254@gmail.com>
2023-12-26 07:45:34 +0300

    spinner

--- app/home/loading.tsx
+++ app/home/loading.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import React from 'react'
+import { Spinner } from '@/components/common';
 
 export default function Loading() {
   return (
-    <div>
-        Loading...
-      
+    <div className='flex justify-center my-8'>
+      <Spinner lg />
     </div>
   )
 }
+
--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -141,38 +141,38 @@ export default function Navbar() {
 											leaveTo="transform opacity-0 scale-95"
 										>
 											<Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-											<Menu.Item>
-												{({ active }) => (
-												<a
-													href="/profile"
-													className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-												>
-													Your Profile
-												</a>
-												)}
-											</Menu.Item>
-											{/* <Menu.Item>
-												{({ active }) => (
-												<a
-													href='/'
-													className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-												>
-													Settings
-												</a>
-												)}
-											</Menu.Item>      */}
-											<Menu.Item>
-												{({ active }) => (
-												<a
-													href="/"
-													onClick={handleLogout}
-													className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-												>
-													Sign Out
-													
-												</a>
-												)}
-											</Menu.Item>
+												<Menu.Item>
+													{({ active }) => (
+													<a
+														href="/profile"
+														className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+													>
+														Your Profile
+													</a>
+													)}
+												</Menu.Item>
+												{/* <Menu.Item>
+													{({ active }) => (
+													<a
+														href='/'
+														className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+													>
+														Settings
+													</a>
+													)}
+												</Menu.Item>      */}
+												<Menu.Item>
+													{({ active }) => (
+													<a
+														href="/"
+														onClick={handleLogout}
+														className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+													>
+														Sign Out
+														
+													</a>
+													)}
+												</Menu.Item>
 											</Menu.Items>
 										</Transition>
 									</Menu>
