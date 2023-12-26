4ba62e3e2aceb3d499ab7e2a0dc2c902c057fbe4
reelcollins <elijahcollins254@gmail.com>
2023-12-26 17:39:20 +0300

    use client

--- app/navbar/page.tsx
+++ app/navbar/page.tsx
@@ -1,7 +1,11 @@
+'use client';
+
+
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import cn from 'classnames';
+import { createContext } from 'react';
 
 const navigation = [
   { name: 'Dashboard', href: '#', current: true },
