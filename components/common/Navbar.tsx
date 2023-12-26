41fae9508f14c4604db1df7267a26e9787ba8b40
reelcollins <elijahcollins254@gmail.com>
2023-12-26 18:47:46 +0300

    !ismobile

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -38,7 +38,7 @@ export default function Navbar() {
 		<>
 			<NavLink
 				isSelected={isSelected('/home')}
-				isMobile={isMobile}
+				// isMobile={isMobile}
 				href='/home'
 			>
 				Home
@@ -51,14 +51,14 @@ export default function Navbar() {
 		<>
 			<NavLink
 				isSelected={isSelected('/auth/login')}
-				isMobile={isMobile}
+				// isMobile={isMobile}
 				href='/auth/login'
 			>
 				Login
 			</NavLink>
 			<NavLink
 				isSelected={isSelected('/auth/register')}
-				isMobile={isMobile}
+				// isMobile={isMobile}
 				href='/auth/register'
 			>
 				Register
@@ -71,7 +71,7 @@ export default function Navbar() {
 			{({ open }) => (
 				<>
 					<div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 '>
-						<div className='relative flex h-16 items-center'>
+						<div className='relative flex h-16 items-center justify-between'>
 							<div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
 								<Disclosure.Button className='inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
 									<span className='sr-only'>
