103377a76477db4929f355e075cf087de1a12991
reelcollins <elijahcollins254@gmail.com>
2023-12-26 07:50:27 +0300

    bell icon

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -8,7 +8,7 @@ function classNames(...classes: string[]): string {
 
 import { usePathname } from 'next/navigation';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import { useAppSelector, useAppDispatch } from '@/redux/hooks';
 import { useLogoutMutation } from '@/redux/features/authApiSlice';
 import { logout as setLogout } from '@/redux/features/authSlice';
@@ -115,6 +115,15 @@ export default function Navbar() {
 									</div>
 								</div>
 								<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+
+									<button
+									type="button"
+									className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+									>
+										<span className="absolute -inset-1.5" />
+										<span className="sr-only">View notifications</span>
+										<BellIcon className="h-6 w-6" aria-hidden="true" />
+									</button>
 
 									{/* Profile dropdown */}
 									<Menu as="div" className="relative ml-3">
