249097a6cf6cf388816a5536ae352be1747b9a1c
reelcollins <elijahcollins254@gmail.com>
2023-12-27 19:53:45 +0300

    400

--- components/common/PropertyCard.tsx
+++ components/common/PropertyCard.tsx
@@ -54,7 +54,7 @@ export default function PropertyCard({
             <Link href={{ pathname: `/home/${slug}`, query: { slug: slug }}}>
                 <Box
                 backgroundImage = {`url("${main_photo}")`}
-                height="500px"
+                height="400px"
                 backgroundPosition="center center"
                 backgroundSize="cover"
                 position="relative"
