426adf4077d0c10414a86c12802804443a226e32
reelcollins <elijahcollins254@gmail.com>
2024-01-02 15:43:27 +0300

    Bucket not defined

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,12 +1,17 @@
-import { createPresignedPost } from '@aws-sdk/s3-presigned-post'
-import { S3Client } from '@aws-sdk/client-s3'
-import { v4 as uuidv4 } from 'uuid'
+import { createPresignedPost } from '@aws-sdk/s3-presigned-post';
+import { S3Client } from '@aws-sdk/client-s3';
+import { v4 as uuidv4 } from 'uuid';
 
 export async function POST(request: Request) {
-  const { filename, contentType } = await request.json()
+  const { filename, contentType } = await request.json();
 
   try {
-    const client = new S3Client({ region: process.env.AWS_REGION })
+    // Check if AWS_REGION and AWS_BUCKET_NAME are defined
+    if (!process.env.AWS_REGION || !process.env.AWS_BUCKET_NAME) {
+      throw new Error('AWS_REGION or AWS_BUCKET_NAME not defined in environment variables.');
+    }
+
+    const client = new S3Client({ region: process.env.AWS_REGION });
     const { url, fields } = await createPresignedPost(client, {
       Bucket: process.env.AWS_BUCKET_NAME,
       Key: uuidv4(),
@@ -19,10 +24,10 @@ export async function POST(request: Request) {
         'Content-Type': contentType,
       },
       Expires: 600, // Seconds before the presigned post expires. 3600 by default.
-    })
+    });
 
-    return Response.json({ url, fields })
+    return Response.json({ url, fields });
   } catch (error) {
-    return Response.json({ error: error.message })
+    return Response.json({ error: error.message });
   }
 }
