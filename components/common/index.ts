026e82590f6f72b5fee0087fff4ae3671a71990e
reelcollins <elijahcollins254@gmail.com>
2024-01-02 18:26:12 +0300

    reset

--- components/common/index.ts
+++ components/common/index.ts
@@ -13,4 +13,4 @@ export { default as PropertyYoutubeEmbed } from './PropertyYoutubeEmbed';
 export { default as PropertyMatterPortEmbed } from './PropertyMatterPortEmbed';
 export { default as PropertiesMap } from './PropertiesMap';
 export { default as UploadMap } from './UploadMap';
-export { default as ImageUpload } from './ImageUpload';
+// export { default as ImageUpload } from './ImageUpload';
