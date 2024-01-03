53d32298e42a03c504507dd9b20cd991b7f1842e
reelcollins <elijahcollins254@gmail.com>
2024-01-04 01:34:01 +0300

    uploadimage debug

--- app/uploadimage/page.tsx
+++ app/uploadimage/page.tsx
@@ -1,65 +1,21 @@
 'use client'
 
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { useDispatch } from 'react-redux';
-import { addObjectUrls } from '@/redux/features/objectUrlsSlice';
+import { useState } from 'react'
 
-import styles from '@/styles/Map.module.css';
 export default function Page() {
-  const [mainPhoto, setMainPhoto] = useState<File | null>(null);
-  const [additionalPhotos, setAdditionalPhotos] = useState<File[]>([]);
-  const [uploading, setUploading] = useState(false);
-  const [objectUrls, setObjectUrls] = useState<string[]>([]);
-  const router = useRouter();
-  const dispatch = useDispatch();
-
-  
-  
+  const [file, setFile] = useState<File | null>(null)
+  const [uploading, setUploading] = useState(false)
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    
-    e.preventDefault();
+    e.preventDefault()
 
-    if (!mainPhoto) {
-      alert('Please select a main photo to upload.');
-      return;
+    if (!file) {
+      alert('Please select a file to upload.')
+      return
     }
 
-    setUploading(true);
+    setUploading(true)
 
-    try {
-      // Upload main photo
-      const mainPhotoUrl = await uploadFile(mainPhoto);
-      setObjectUrls([mainPhotoUrl]);
-
-      // Upload additional photos
-      const additionalPhotoUrls = await Promise.all(
-        additionalPhotos.map(async (file) => uploadFile(file))
-      );
-      setObjectUrls((prevUrls) => [...prevUrls, ...additionalPhotoUrls]);
-
-      alert('All uploads successful!');
-
-      
-      
-      const objectUrls = [mainPhotoUrl, ...additionalPhotoUrls];
-      
-      dispatch(addObjectUrls(objectUrls));
-      console.log(objectUrls)
-      
-      router.push('/realtor/upload');
-
-
-    } catch (error) {
-      console.error('Upload error:', error);
-      alert('Upload failed.');
-    } finally {
-      setUploading(false);
-    }
-  };
-
-  const uploadFile = async (file: File): Promise<string> => {
     const response = await fetch(
       process.env.NEXT_PUBLIC_BASE_URL + '/api/upload',
       {
@@ -69,71 +25,54 @@ export default function Page() {
         },
         body: JSON.stringify({ filename: file.name, contentType: file.type }),
       }
-    );
+    )
 
     if (response.ok) {
-      const { url, fields } = await response.json();
+      const { url, fields } = await response.json()
 
-      const formData = new FormData();
+      const formData = new FormData()
       Object.entries(fields).forEach(([key, value]) => {
-        formData.append(key, value as string);
-      });
-      formData.append('file', file);
+        formData.append(key, value as string)
+      })
+      formData.append('file', file)
 
       const uploadResponse = await fetch(url, {
         method: 'POST',
         body: formData,
-      });
+      })
 
       if (uploadResponse.ok) {
-        // alert('Upload successful!');
-
-        const objectKey = fields.key;
-        const objectUrl = `${url}${objectKey}`;
-        return objectUrl
-
+        alert('Upload successful!')
       } else {
-        console.error('S3 Upload Error:', uploadResponse);
-        alert('Upload failed.');
-        throw new Error('Upload failed.');
+        console.error('S3 Upload Error:', uploadResponse)
+        alert('Upload failed.')
       }
     } else {
-      console.error('Failed to get pre-signed URL.');
-      alert('Failed to get pre-signed URL.');
-      throw new Error('Failed to get pre-signed URL.');
+      alert('Failed to get pre-signed URL.')
     }
-  };
 
-  // Pass objectUrls to Property upload page or use them as needed
+    setUploading(false)
+  }
 
   return (
-    <main className={styles.main}>
-      <h1 className={styles.h1}>Upload Photos to S3</h1>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <label htmlFor="mainPhoto">Main Photo:</label>
+    <main>
+      <h1>Upload a File to S3</h1>
+      <form onSubmit={handleSubmit}>
         <input
-          className={styles.input}
-          id="mainPhoto"
+          id="file"
           type="file"
-          onChange={(e) => setMainPhoto(e.target.files?.[0] || null)}
+          onChange={(e) => {
+            const files = e.target.files
+            if (files) {
+              setFile(files[0])
+            }
+          }}
           accept="image/png, image/jpeg"
         />
-
-        <label htmlFor="additionalPhotos">Additional Photos:</label>
-        <input
-          className={styles.input}
-          id="additionalPhotos"
-          type="file"
-          onChange={(e) => setAdditionalPhotos(Array.from(e.target.files || []))}
-          accept="image/png, image/jpeg"
-          multiple
-        />
-
-        <button className={styles.button} type="submit" disabled={uploading}>
+        <button type="submit" disabled={uploading}>
           Upload
         </button>
       </form>
     </main>
-  );
+  )
 }
-
