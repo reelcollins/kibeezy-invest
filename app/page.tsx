236f31de9ed6606ec16e17486ee928d4f81cf724
reelcollins <elijahcollins254@gmail.com>
2025-01-11 12:27:17 +0300

    packages

--- app/page.tsx
+++ app/page.tsx
@@ -98,12 +98,13 @@ export default function Page() {
       </div>
       <div className='mt-12 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3'>
         {[
-          // { name: 'Basic', description: '5 for 30min', color: 'blue' },
+          
           { name: 'Standard', description: '10 for 1', color: 'green' },
           { name: 'Premium', description: '20 for 3hrs', color: 'purple' },
           { name: 'Ultimate', description: '30 for 6.5hrs', color: 'red' },
           { name: 'Economy', description: '50 for 12hrs', color: 'yellow' },
           { name: 'Business', description: '80 for 24hrs', color: 'indigo' },
+          { name: 'Basic', description: '100 for 2 days', color: 'blue' },
         ].map((pkg) => (
           <div
             key={pkg.name}
