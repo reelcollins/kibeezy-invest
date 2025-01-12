8539348e876750f79c58c18f70ce8e60b26141a1
reelcollins <elijahcollins254@gmail.com>
2025-01-12 23:01:15 +0300

    contact

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,12 +1,13 @@
 export default function Footer() {
 	return (
-		<footer className='bg-gray-100 h-16'>
-			<div className='h-full px-2'>
-				<div className='flex items-center justify-center h-full'>
-					<p className='text-gray-400 text-xs'>
-						&copy; 2025 KIBEEZY TECHNOLOGIES LLC. All rights reserved.
-					</p>
-				</div>
+		<footer className="bg-gray-100 h-16">
+			<div className="h-full px-4 flex items-center justify-between">
+				<p className="text-gray-400 text-xs">
+					&copy; 2025 KIBEEZY TECHNOLOGIES LLC. All rights reserved.
+				</p>
+				<p className="text-gray-600 text-sm flex items-center gap-2">
+					📞 <a href="tel:0116293558" className="hover:underline">011 629 3558</a>
+				</p>
 			</div>
 		</footer>
 	);
