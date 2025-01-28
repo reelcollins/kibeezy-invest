f295f6c189ec265a56016885b92311a0478d57d4
reelcollins <elijahcollins254@gmail.com>
2025-01-28 12:28:05 +0300

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,48 +4,49 @@ import type { Metadata } from 'next';
 import Provider from '@/redux/provider';
 import { Footer, Navbar } from '@/components/common';
 import { Setup } from '@/components/utils';
-import { Providers } from './providers'
+import { Providers } from './providers';
 import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes';
 import { register } from 'swiper/element/bundle';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 register();
-import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 const inter = Inter({
-	subsets: ['latin'] });
+  subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
-	title: 'Kibeezy WiFi',
-	description: 'Invest page',
-	};
+  title: 'Kibeezy WiFi',
+  description: 'Invest page',
+};
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang='en'>
-			<body className={inter.className}>
-				<Provider>
-					<UserProvider>
-						<Providers>
-							
-								<Theme>
-									<Setup />
-									<Navbar />
-									<div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 my-8'>
-										{children}
-									</div>
-									<Footer />
-								</Theme>
-							
-						</Providers>
-					</UserProvider>
-				</Provider>
-
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <head>
+        {/* Add the favicon link */}
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </head>
+      <body className={inter.className}>
+        <Provider>
+          <UserProvider>
+            <Providers>
+              <Theme>
+                <Setup />
+                <Navbar />
+                <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 my-8">
+                  {children}
+                </div>
+                <Footer />
+              </Theme>
+            </Providers>
+          </UserProvider>
+        </Provider>
+      </body>
+    </html>
+  );
 }
-
