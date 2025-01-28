12110e616d2c2440a13f482e631062771e53740c
reelcollins <elijahcollins254@gmail.com>
2025-01-28 12:22:31 +0300

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const inter = Inter({
 
 export const metadata: Metadata = {
 	title: 'Kibeezy WiFi',
-	description: 'Kibeezy WiFi Welcome page',
+	description: 'Invest page',
 	};
 
 export default function RootLayout({
